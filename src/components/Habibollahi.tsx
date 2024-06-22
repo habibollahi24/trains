0841e197fb1894df293c4f2d3b10ca0aa7306933
mohammad <habibollahi24@gmail.com>
2024-06-22 13:26:03 +0330

    add habibollahi

---
+++ src/components/Habibollahi.tsx
@@ -0,0 +1,3 @@
+export default function Habibollahi() {
+  return <div>Habibollahi - mohammad</div>;
+}
--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,4 +1,5 @@
 import Footer from '@/components/Footer';
+import Habibollahi from '@/components/Habibollahi';
 import Navbar from '@/components/Navbar';
 import SubHeader from '@/components/SubHeader';
 
@@ -6,6 +7,7 @@ function HomePage() {
   return (
     <>
       <Navbar />
+      <Habibollahi />
       <p>hello react query and git hub</p>
       <SubHeader />
       <p>hello react query</p>
