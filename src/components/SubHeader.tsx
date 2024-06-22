bd6adc17e669f065daf20048a58a83e760aa84e3
mohammad <habibollahi24@gmail.com>
2024-06-22 13:12:41 +0330

    add sub header

---
+++ src/components/SubHeader.tsx
@@ -0,0 +1,7 @@
+
+
+export default function SubHeader() {
+  return (
+    <div>SubHeader</div>
+  )
+}
--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,10 +1,12 @@
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
+import SubHeader from '@/components/SubHeader';
 
 function HomePage() {
   return (
     <>
       <Navbar />
+      <SubHeader />
       <p>hello react query</p>
       <Footer />
     </>
